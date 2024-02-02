491f344d34a157fb4d7810dcc617ce9279876e5a
DarkGloves <110231449+DarkGloves@users.noreply.github.com>
2024-02-02 20:26:00 +0100

    Responsive frames tab

--- src/gui.py
+++ src/gui.py
@@ -156,6 +156,7 @@ class GUI(CTk):
     def _performanceTab(self, tabName:str):
         frame = CTkScrollableFrame(self.tabview.tab(tabName))
         frame.grid(row=0, column=0, sticky=NSEW, padx=5, pady=5)
+        frame.columnconfigure((0), weight=1)
         self.tabview.tab(tabName).rowconfigure(0, weight=1)  # Configure row weight
         self.tabview.tab(tabName).columnconfigure(0, weight=1)  # Configure column weight
 
